006b0fca1a402dad4cc5128e2eb5a37bbc0c5bb2
Andreas <andrmj@uio.no>
2022-09-23 16:47:33 +0200

    Update part1.py

--- Del3/part1.py
+++ Del3/part1.py
@@ -25,17 +25,29 @@ p_vel = system.initial_velocities
 radius = 8961621.4961 
 def flux_rec(dist):
     total_flux = 4*np.pi*((system.star_radius*1e3)**2)*const.sigma*system.star_temperature**4
-    print(total_flux)
+    #print(total_flux)
     fluxg = total_flux/(4*np.pi*dist**2)
     return fluxg
 
 print(flux_rec(utils.AU_to_m(p_pos[0,0])))
-#fr = flux_rec(p_pos[0,0])
+fr = flux_rec(utils.AU_to_m(p_pos[0,0]))
 
 #d_a = fr/(np.pi*radius**2)
 #print(d_a)
 
-#a = 40*0.12/4.7
-#print(a)
+a = (40/0.12)/2900
+print(a)
 
+### 1.3 
+#flux_rec(r)*2*np.pi*R**2
 
+### 1.4
+def temps():
+    temps = np.zeros(7)
+    for i in range(7):
+        temps[i] = np.power((flux_rec(utils.AU_to_m(np.linalg.norm([p_pos[0,i],p_pos[1,i]])))/(const.sigma)),1/4)
+    return temps 
+
+### 1.5 
+ts = temps()
+print(ts) #habit = #2 , #6
