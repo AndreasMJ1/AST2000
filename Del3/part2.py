28ffe9dccf28787e9fdd39acd4d65c61f8a34e2b
Andreas <andrmj@uio.no>
2022-09-25 17:26:07 +0200

    1

--- Del3/part2.py
+++ Del3/part2.py
@@ -4,4 +4,4 @@ from part1 import *
 
 sqr_m_solarpanel1 = (40/0.12)/flux_rec(np.linalg.norm([utils.AU_to_m(p_pos[0,2]),utils.AU_to_m(p_pos[1,2])]))
 
-print(sqr_m_solarpanel1)
+print(sqr_m_solarpanel1) #0.5259362817340424 m^2
