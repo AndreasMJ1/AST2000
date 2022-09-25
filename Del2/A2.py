ba6ea52303f578490bc90313893b69a55b69534e
Andreas <andrmj@uio.no>
2022-09-25 17:06:52 +0200

    1

--- Del2/A2.py
+++ Del2/A2.py
@@ -73,27 +73,28 @@ color_list = ['Firebrick','Chartreuse','Khaki','Sienna','CornflowerBlue','Teal',
 
 r,v,a_chk,cnt,push_p = sim_orbits(119150,0.0002)  #Unpacking simulation 
 
-for i in range(7):                                #Plotting Exact solution
-    plt.plot(analytic_orbits(m_ax[i],ecc[i],aph_ang[i],119000,p_pos[0][i],p_pos[1][i])[0],analytic_orbits(m_ax[i],ecc[i],aph_ang[i]
-    ,119000,p_pos[0][i],p_pos[1][i])[1],linestyle='dotted',color = f'{color_list[i]}')
+if __name__ == '__main__':
+        
+    for i in range(7):                                #Plotting Exact solution
+        plt.plot(analytic_orbits(m_ax[i],ecc[i],aph_ang[i],119150,p_pos[0][i],p_pos[1][i])[0],analytic_orbits(m_ax[i],ecc[i],aph_ang[i]
+        ,119150,p_pos[0][i],p_pos[1][i])[1],linestyle='dotted',color = f'{color_list[i]}')
 
-print((a_chk[0][0],a_chk[7][0],a_chk[0][0]-a_chk[7][0],a_chk[0][1],a_chk[7][1],a_chk[0][2],a_chk[7][2])) #(-8.282099940755405e-12, 0.0012898681485006699, 0.0012898732900769251, 6.449340742503349, 6.449366450384625)
-
-
-print(f"Kepler {119000*0.0002/20,np.sqrt(m_ax[0]**3)}")
-print(f"Netwon {119000*0.0002/20,np.sqrt((4*np.pi**2)/(4*np.pi**2*(system.star_mass+system.masses[0]))*m_ax[0]**3),}") #595*0.002
-
-print(system.types)
-
-plt.scatter(0,0,color = 'black')                  #Plotting simulation 
-for i in range(7):
-    plt.plot(r[:,i,0],r[:,i,1],color = f'{color_list[i]}')
-plt.legend(['0','1','2','3','4','5','6'])
-plt.xlabel('Distance (AU)')
-plt.ylabel('Distance (AU)')
-plt.title('Comparing analytical and numeric calculations')
-plt.show()
+    print((a_chk[0][0],a_chk[7][0],a_chk[0][0]-a_chk[7][0],a_chk[0][1],a_chk[7][1],a_chk[0][2],a_chk[7][2])) #(-8.282099940755405e-12, 0.0012898681485006699, 0.0012898732900769251, 6.449340742503349, 6.449366450384625)
 
 
-rshape = np.reshape(r,(2,7,119150))
-mission.verify_planet_positions(119150*0.0002, rshape)
+    print(f"Kepler {119150*0.0002/20,np.sqrt(m_ax[0]**3)}")
+    print(f"Netwon {119150*0.0002/20,np.sqrt((4*np.pi**2)/(4*np.pi**2*(system.star_mass+system.masses[0]))*m_ax[0]**3),}") #595*0.002
+
+    print(system.types)
+
+    plt.scatter(0,0,color = 'black')                  #Plotting simulation 
+    for i in range(7):
+        plt.plot(r[:,i,0],r[:,i,1],color = f'{color_list[i]}')
+    plt.legend(['0','1','2','3','4','5','6'])
+    plt.xlabel('Distance (AU)')
+    plt.ylabel('Distance (AU)')
+    plt.title('Comparing analytical and numeric calculations')
+    plt.show()
+
+    rshape = np.reshape(r,(2,7,119150))
+    mission.verify_planet_positions(119150*0.0002, rshape)
