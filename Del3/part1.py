dbbe20ecf66080c36d2f602616f5163a74f75fc2
Andreas <andrmj@uio.no>
2022-09-25 16:50:17 +0200

    Update part1.py

--- Del3/part1.py
+++ Del3/part1.py
@@ -19,35 +19,37 @@ m_ax = system.semi_major_axes
 p_pos = system.initial_positions
 p_vel = system.initial_velocities
 
-#print(system.star_temperature)
-#Lum = 4*np.pi*system.star_radii**2*const.sigma**4 
-#print((system.star_radius*1e3)**2)
-radius = 8961621.4961 
+radius = 8961621.4961
 def flux_rec(dist):
-    total_flux = 4*np.pi*((system.star_radius*1e3)**2)*const.sigma*system.star_temperature**4
-    #print(total_flux)
-    fluxg = total_flux/(4*np.pi*dist**2)
-    return fluxg
-
-print(flux_rec(utils.AU_to_m(p_pos[0,0])))
-fr = flux_rec(utils.AU_to_m(p_pos[0,0]))
-
-#d_a = fr/(np.pi*radius**2)
-#print(d_a)
-
-a = (40/0.12)/2900
-print(a)
-
-### 1.3 
-#flux_rec(r)*2*np.pi*R**2
-
-### 1.4
+        total_flux = 4*np.pi*((system.star_radius*1e3)**2)*const.sigma*system.star_temperature**4
+        #print(total_flux)
+        fluxg = total_flux/(4*np.pi*dist**2)
+        return fluxg
+        
 def temps():
-    temps = np.zeros(7)
-    for i in range(7):
-        temps[i] = np.power((flux_rec(utils.AU_to_m(np.linalg.norm([p_pos[0,i],p_pos[1,i]])))/(const.sigma)),1/4)
-    return temps 
+        temps = np.zeros(7)
+        for i in range(7):
+            temps[i] = np.power((flux_rec(utils.AU_to_m(np.linalg.norm([p_pos[0,i],p_pos[1,i]])))/(const.sigma)),1/4)
+        return temps 
 
-### 1.5 
-ts = temps()
-print(ts) #habit = #2 , #6
+
+if __name__ == '__main__': 
+    ### 1.1 
+    #flux_rec()
+    print(flux_rec(utils.AU_to_m(p_pos[0,0])))
+    fr = flux_rec(utils.AU_to_m(p_pos[0,0]))
+
+    ### 1.2 
+    sqr_m_solarpanel = (40/0.12)/2900 
+    print(sqr_m_solarpanel)      
+
+    ### 1.3 
+    #flux_rec(r)*2*np.pi*R**2
+
+    ### 1.4
+    #temps()
+
+    ### 1.5 
+    ts = temps()
+    print(ts)     #habitable = #3 , #7
+    flux_rec(utils.AU_to_m(p_pos[0,0]))
