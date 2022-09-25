16c0f95c880919d408f0de6191e8e3f4ff4c170c
Andreas <andrmj@uio.no>
2022-09-25 16:57:55 +0200

    Update part2.py

--- Del3/part2.py
+++ Del3/part2.py
@@ -1,4 +1,7 @@
 from part1 import *
 
+### Velger planet 2 
 
-print(flux_rec(utils.AU_to_m(p_pos[0,0])))
+sqr_m_solarpanel1 = (40/0.12)/flux_rec(np.linalg.norm([utils.AU_to_m(p_pos[0,2]),utils.AU_to_m(p_pos[1,2])]))
+
+print(sqr_m_solarpanel1)
