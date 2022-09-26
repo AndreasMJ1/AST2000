2c2c58f9d33ecd68bec14919aea056b349e60812
Andreas <andrmj@uio.no>
2022-09-26 16:53:27 +0200

    Update part3.py

--- Del3/part3.py
+++ Del3/part3.py
@@ -1,5 +1,6 @@
 from launch_func import* 
 
+
 mission.set_launch_parameters(mean_force*1.6e13, fuel_consume, spacecraft_mass, 500, pos0, 0)
 mission.launch_rocket()
 
@@ -7,9 +8,11 @@ mission.launch_rocket()
 x,l,exiting,tf = particle_sim(x,v,l,exiting,f)
 vel, time1, pos,spmass = orbit_launch(mean_force*1.6e13,spacecraft_mass,fuel_consume)
 
-print(m)
-fuellc = particles_per_second * 1.6E13*m
-print(fuellc)
+def gen_launch(time, pos, planet):
+    vesc = 
+    while v >= vesc:
+
+
 
 #print(time1[-1])
 #print(vel)
