a1f7fe96a9686d3664b11aad4d71cb32a5017a46
Andreas <andrmj@uio.no>
2022-10-12 14:43:30 +0200

    Create PartA.py

---
+++ Del5/PartA.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as sp
+import ast2000tools.constants as const
+from tqdm import trange
+from numba import njit
+import ast2000tools.utils as utils
+from ast2000tools.solar_system import SolarSystem
+from ast2000tools.space_mission import SpaceMission
+from PIL import Image
+
+seed = utils.get_seed('andrmj')
+mission = SpaceMission(seed)
+system = SolarSystem(seed)
+
+ecc = system.eccentricities
+aph_ang = system.aphelion_angles
+m_ax = system.semi_major_axes
+p_pos = system.initial_positions
+p_vel = system.initial_velocities
+p_masses = system.masses
+p_radii = system.radii
