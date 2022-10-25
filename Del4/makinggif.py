7bc3c564502d04dd3bfbf1d6b4e89ab3a6375db5
Andreas <andrmj@uio.no>
2022-10-25 03:04:36 +0200

    1

---
+++ Del4/makinggif.py
@@ -0,0 +1,11 @@
+import imageio
+import numpy as np 
+from PIL import Image
+from tqdm import trange
+images = []
+for i in trange(360):
+    img = Image.open(f"C:/Users/andym/Documents/GitHub/AST2000/Del4/360/{i}.png")
+    pix = np.array(img)
+    images.append(pix)
+
+imageio.mimsave("movie.gif",images)
