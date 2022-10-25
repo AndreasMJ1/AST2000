477285cd8796e728691a19ee0a760f0451db9729
Andreas <andrmj@uio.no>
2022-10-25 11:54:19 +0200

    1

--- Del4/makinggif.py
+++ Del4/makinggif.py
@@ -3,6 +3,8 @@ import numpy as np
 from PIL import Image
 from tqdm import trange
 images = []
+
+
 for i in trange(360):
     img = Image.open(f"C:/Users/andym/Documents/GitHub/AST2000/Del4/360/{i}.png")
     pix = np.array(img)
